00c9167c1ebac4b765d8a13ca963f8d3ce354c0e
LAPTOP-NDL3QSB6\abdur-rehman <abdur4082@gmail.com>
2021-08-01 21:33:06 +0500

    Remove unused imports in card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import Icon from "@material-ui/core/Icon";
 import DashboardIcon from "../icons/Icon";
 const useStyles = makeStyles({
   root: {
